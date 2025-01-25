e43757fe147f51a56a604828268532dea54cc387
rohithagannamaneedi <rohithagannamaneedi@gmail.com>
2025-01-25 16:24:06 +0530

    done

---
+++ index.js
@@ -0,0 +1,25 @@
+const movies = document.getElementById("movies")
+function fetchMovies(){
+    fetch("http://www.omdbapi.com/?i=tt3896198&apikey=8dd35b34&s=avengers")
+    .then((data)=>{
+        return data.json();
+})
+.then((data)=>{
+    data.Search.forEach((ele)=>{
+    const div = document.createElement("div");
+    div.className = "movie-card"
+    const img = document.createElement("img");
+    const h3 = document.createElement("h3");
+    img.src=ele.Poster;
+    h3.innerText=ele.Title;
+    div.append(img,h3);
+    movies.appendChild(div);
+    })
+    
+    console.log(data);
+})
+.catch((err)=>{
+    comsole.log(arr)
+})
+}
+fetchMovies();
